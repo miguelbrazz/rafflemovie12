c89b39acbff724ef6611943d0608daac86b6fb5e
miguelbrazz <msottom2@gmail.com>
2023-05-15 18:06:27 -0300

    first commit1

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/raffle-movies-website/",
+  base: "/rafflemovie12/",
   plugins: [react()]
 })
